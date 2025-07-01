12031930ef2ba763ac86b5d0a2c19a286ffee81b
jkmdale <jamie.dale@outlook.com>
2025-07-01 22:05:29 +1200

    Update secureStore.ts

--- src/modules/storage/secureStore.ts
+++ src/modules/storage/secureStore.ts
@@ -1,4 +1,4 @@
-// src/modules/storage/secureStore.ts import { openDB } from 'idb' import { aesEncrypt, getCryptoKey } from '@/utils/crypto' import type { Transaction } from '@/types/Transaction'
+// src/modules/storage/secureStore.ts import { openDB } from 'idb' import { aesEncrypt, aesDecrypt, getCryptoKey } from '@/utils/crypto' import type { Transaction } from '@/types/Transaction'
 
 const DB_NAME = 'SFAI-secure' const STORE_NAME = 'transactions'
 
@@ -7,4 +7,8 @@ export async function encryptAndStoreTransactions(data: Transaction[]): Promise<
 const db = await openDB(DB_NAME, 1, { upgrade(db) { if (!db.objectStoreNames.contains(STORE_NAME)) { db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true }) } } })
 
 const tx = db.transaction(STORE_NAME, 'readwrite') const store = tx.objectStore(STORE_NAME) encrypted.forEach(enc => store.add(enc)) await tx.done }
+
+export async function decryptAndLoadTransactions(): Promise<Transaction[]> { const key = await getCryptoKey() const db = await openDB(DB_NAME, 1) const tx = db.transaction(STORE_NAME, 'readonly') const store = tx.objectStore(STORE_NAME)
+
+const all = await store.getAll() const decrypted = await Promise.all(all.map(entry => aesDecrypt<Transaction>(entry, key))) return decrypted }
 
