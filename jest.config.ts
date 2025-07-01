1c686a26adc33143054b25976aa88917b4006ca7
jkmdale <jamie.dale@outlook.com>
2025-07-01 21:35:56 +1200

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,18 @@
+// jest.config.ts
 import type { Config } from 'jest'
 
 const config: Config = {
   verbose: true,
   preset: 'ts-jest',
-  testEnvironment: 'jsdom', // This is what you'll be using for front-end tests
+  testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy' // Useful for mocking CSS imports
+    '^@parsers/(.*)$': '<rootDir>/src/modules/import/parsers/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
   globals: {
     'ts-jest': {
