724b85b8466fb93b8f1e40d4b8cf57847c7d1bbc
jkmdale <jamie.dale@outlook.com>
2025-07-01 22:01:53 +1200

    Create storage/secureStore.ts

---
+++ src/modules/storage/secureStore.ts
@@ -0,0 +1,10 @@
+// src/modules/storage/secureStore.ts import { openDB } from 'idb' import { aesEncrypt, getCryptoKey } from '@/utils/crypto' import type { Transaction } from '@/types/Transaction'
+
+const DB_NAME = 'SFAI-secure' const STORE_NAME = 'transactions'
+
+export async function encryptAndStoreTransactions(data: Transaction[]): Promise<void> { const key = await getCryptoKey() const encrypted = await Promise.all(data.map(d => aesEncrypt(d, key)))
+
+const db = await openDB(DB_NAME, 1, { upgrade(db) { if (!db.objectStoreNames.contains(STORE_NAME)) { db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true }) } } })
+
+const tx = db.transaction(STORE_NAME, 'readwrite') const store = tx.objectStore(STORE_NAME) encrypted.forEach(enc => store.add(enc)) await tx.done }
+
